ea3511b1ff569410636a333086bb463d197d2e5b
Alysa654 <Alysasolomon@outlook.com>
2026-02-18 19:32:10 -0600

    Added SaveUtility and Edited some files

---
+++ SaveUtility.java
@@ -0,0 +1,21 @@
+// Made by Alysa Solomon
+
+//imports to write files and read/write JSON objects
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+
+class SaveUtility {
+
+	//Converts a give Recipe to a JSON file
+	public bool convertToJSON(Recipe r){
+		JSONObject jsonObj = new JSONObject();
+		/* TODO: add the ingredient name/type to the JSON object
+		then all of the steps
+		then all the ingredients and types
+		Follow the JSON file "Sample_Recipe.json" for template
+		DO NOT SAVE TO "Sample_Recipe.json"
+		REPEAT DO NOT SAVE TO "Sample_Recipe.json"
+		*/
+	}
+}
