ac04f9259be0df6026f6de909670e234c5f95073
IRBGit <irob05@comcast.net>
2026-02-25 16:19:49 -0600

    blah blah blah

--- RRG/RRG/src/main/java/com/RRG/model/Recipe.java
+++ RRG/RRG/src/main/java/com/RRG/model/Recipe.java
@@ -1,15 +1,26 @@
 package com.RRG.model;
 
+import java.util.Map;
+
 public class Recipe {
     private int id;
     private String name;
+    private String category;
+    private String area;
+    private Map<String, Ingredient> ingredients;
     private String instructions;
+    private String[] tags;
+    private String video;
 
-    public Recipe(int id, String name, String instructions)
-    {
-        this.id = id;
-        this.name = name;
-        this.instructions = instructions;
+    public Recipe(int id, String name, String category, String area, Map<String, Ingredient> ingredients, String instructions, String[] tags, String video){
+        id=this.id;
+        name=this.name;
+        category=this.category;
+        area=this.area;
+        ingredients=this.ingredients
+        instructions=this.instructions
+        tags=this.tags
+        video=this.video
+        
     }
-
 }
