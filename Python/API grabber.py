c213a1ff7d9e4a3d260bd41805a530bd7b5a14f1
IRBGit <irob05@comcast.net>
2026-03-30 14:47:20 -0500

    created a list of all ingredients possible in a recipe

--- Python/API grabber.py
+++ Python/API grabber.py
@@ -1,10 +1,11 @@
 import requests
-#import filewriter
 def main():
     r=requests.get('https://www.themealdb.com/api/json/v1/1/list.php?i=list',)
     data = r.json()
-    ingredients = [meal.get("strIngredient") for meal in data.get("meals", [])]
+    ingredients = [meal.get("strIngredient")+"\n" for meal in data.get("meals", [])]
     print(ingredients)
+    with open("ingredients.txt", "w") as f:
+          f.writelines(ingredients)
 if __name__=="__main__":
         main()
     
