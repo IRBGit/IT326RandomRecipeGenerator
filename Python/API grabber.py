467223fb4254bde9a0be6311ed1a77647d1f29a0
IRBGit <irob05@comcast.net>
2026-03-30 16:00:14 -0500

    added complete list of ingredients, categories, and areas, as well as a module to grab them from the API

--- Python/API grabber.py
+++ Python/API grabber.py
@@ -1,10 +1,21 @@
 import requests
-#import filewriter
+def createfile(text, filename):
+      with open(filename+'.txt', 'w') as f:
+            f.writelines(text)
 def main():
     r=requests.get('https://www.themealdb.com/api/json/v1/1/list.php?i=list',)
     data = r.json()
-    ingredients = [meal.get("strIngredient") for meal in data.get("meals", [])]
-    print(ingredients)
+    ingredients = [meal.get("strIngredient")+"\n" for meal in data.get("meals", [])]
+    createfile(ingredients, "ingredients")
+    r=requests.get('https://www.themealdb.com/api/json/v1/1/list.php?c=list',)
+    data = r.json()
+    categories = [meal.get("strCategory")+"\n" for meal in data.get("meals", [])]
+    createfile(categories, "categories")
+    r=requests.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list',)
+    data = r.json()
+    areas = [meal.get("strArea")+"\n" for meal in data.get("meals", [])]
+    createfile(areas, "areas")
+    
 if __name__=="__main__":
         main()
     
