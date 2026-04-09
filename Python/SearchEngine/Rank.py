dea73db464d5096c8e599b06df02dedab9807864
Alysa654 <Alysasolomon@outlook.com>
2026-04-08 20:23:02 -0500

    updated recipe_from_json

--- Python/SearchEngine/Rank.py
+++ Python/SearchEngine/Rank.py
@@ -5,4 +5,7 @@ class Rank:
 
     # returns a sorted list of recipes by time
     def rank_by_newest(self, recipe_list: List[Recipe]):
-        return recipe_list
+        if len(recipe_list) <= 1:
+            return recipe_list
+        sorted = recipe_list.sort(Recipe.published_time)
+        return sorted
