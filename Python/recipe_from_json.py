02d768ac59d513e051dc64cc6149b049ea0c2370
Alysa654 <Alysasolomon@outlook.com>
2026-04-27 13:27:06 -0500

    Update recipe_from_json.py

--- Python/recipe_from_json.py
+++ Python/recipe_from_json.py
@@ -71,5 +71,7 @@ def import_recipes_from_json(file_path):
 if __name__ == "__main__":
     # Ensure this matches your JSON filename
 
-    import_recipes_from_json('Python\\theMealDB_A.json')
-    # recipe_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","Z","0","1","2","3","4","5","6","7","8","9"]
+    # import_recipes_from_json('Python\\theMealDB_A.json')
+    recipe_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","Z","0","1","2","3","4","5","6","7","8","9"]
+    for letter in recipe_list:
+        import_recipes_from_json(f"JSON_Recipes\\{letter}_recipes.json")
