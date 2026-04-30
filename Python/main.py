a6db2d024b809213a6bb67eb4c829fb96a6ee101
Alysa654 <Alysasolomon@outlook.com>
2026-04-30 15:04:50 -0500

    added notes

--- Python/main.py
+++ Python/main.py
@@ -329,6 +329,7 @@ def main():
                     case 2: # Get Pop Searches
                         print("This feature hasn't been implemented yet.")
                     case 3: # Get Random Recipe
+                        # TODO: NOT OUTPUTTING LIST OF RECIPES
                         recipe_list = random_recipe_helper()
                         print(recipe_list)
                     case 4: # Register Account
