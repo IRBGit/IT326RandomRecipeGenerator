b89947889aef770e202c7b6a0f8ee2a412fa6f75
Alysa654 <Alysasolomon@outlook.com>
2026-04-30 10:45:46 -0500

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -31,10 +31,20 @@ def main():
                 print("Please input a valid input.")
                 continue
         else:
-            print("List all possible options in a list here")
+            print("1: Log In")
+            print("2: Get Popular Searches")
+            print("3: Get Random Recipe")
+            print("4: Create New Account")
+            print("5: Search for Recipe")
+            print("6: Exit")
             try:
                 chosen_option = input("Select your choice:")
                 chosen_option = int(chosen_option)
+                if(1 <= chosen_option and chosen_option <= 6):
+                    # switch statement to to pick relevent helper method.
+                    pass
+                else:
+                    print("Invalid Option. Please Try Again.")
             except ValueError:
                 print("Please input a valid input.")
                 continue
