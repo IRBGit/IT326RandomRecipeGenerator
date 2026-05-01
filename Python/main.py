78f0704b4f173f75a4c391300c608d8160ee3353
Ta0845 <AMBALATHANVI123@GMAIL.COM>
2026-05-01 15:37:39 -0500

    Changes to SearchEngine

--- Python/main.py
+++ Python/main.py
@@ -225,7 +225,7 @@ def get_dietary():
             pass
 
 # By Alysa Solomon
-def random_recipe_helper():
+def random_recipe_helper(search_func: SearchEngine.RecipeSearchEngine):
     req_ing = get_ingredients("In Arabic Numerals, How many ingredients do you want to include in the recipe?\n")
     get_value = True
     while get_value:
@@ -235,7 +235,6 @@ def random_recipe_helper():
             get_value = False
         except ValueError:
             print("Please input a valid input")
-    search_func = SearchEngine.RecipeSearchEngine()
     if req_ing == []:
         return search_func.get_random_recipes(choose)
     f = Filter.Filter()
@@ -267,9 +266,8 @@ def search_by_ing(search_func: SearchEngine.RecipeSearchEngine):
 
 
 # By Alysa Solomon
-def search_not_logged_in():
+def search_not_logged_in(search_func: SearchEngine.RecipeSearchEngine):
     is_searching = True
-    search_func = SearchEngine.RecipeSearchEngine()
     while is_searching:
         print("Current Options are listed below:")
         print("1: Search by Name")  # Somewhat Implemented
@@ -355,6 +353,7 @@ def get_pop_searches(service: ServiceContainer):
 #By Alysa Solomon
 def main():
     service = ServiceContainer()
+    search_func = SearchEngine.RecipeSearchEngine(service)
     user = None
     logged_in = False
     will_continue = True
